9293c0698313271e9bb55baf4d7d3df5eb4b1b7c
The-Punisher-droid <60699958+The-Punisher-droid@users.noreply.github.com>
2023-08-15 16:25:59 +0300

    Add files via upload

---
+++ Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        //The ID field is required by the Database fo the Primary Key
+        public int Id { get; set; }
+        //The ? after the string is to indicate that it is nullable
+        public string? Title { get; set; }
+        //A [DataType] Attribute that tells which type of data in ReleaseDate
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre{get; set;}
+        public decimal Price{ get; set; }
+    }
+}
